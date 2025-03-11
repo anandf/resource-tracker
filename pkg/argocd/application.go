3ecade7fd3107362a1db9172684c1f9812e73812
Mangaal <angommeeteimangaal@gmail.com>
2025-03-11 12:34:35 +0530

    use resourcemapperstore
    Signed-off-by: Mangaal <angommeeteimangaal@gmail.com>

--- pkg/argocd/application.go
+++ pkg/argocd/application.go
@@ -3,13 +3,12 @@ package argocd
 import (
 	"context"
 	"fmt"
-	"os"
 
 	"github.com/anandf/resource-tracker/pkg/kube"
+	"github.com/anandf/resource-tracker/pkg/resourcegraph"
 	"github.com/argoproj/argo-cd/v2/common"
-	argocdclient "github.com/argoproj/argo-cd/v2/pkg/apiclient"
-	"github.com/argoproj/argo-cd/v2/pkg/apiclient/application"
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	"github.com/argoproj/argo-cd/v2/pkg/client/clientset/versioned"
 	"github.com/argoproj/argo-cd/v2/reposerver/apiclient"
 	"github.com/argoproj/argo-cd/v2/util/db"
 	kubeutil "github.com/argoproj/argo-cd/v2/util/kube"
@@ -21,54 +20,15 @@ import (
 	"k8s.io/klog/v2"
 )
 
-// Native
-type argoCD struct {
-	Client argocdclient.Client
-}
-
 // ArgoCD is the interface for accessing Argo CD functions we need
 type ArgoCD interface {
 	ListApplications() ([]v1alpha1.Application, error)
+	ProcessApplication(v1alpha1.Application) error
 }
 
 type ResourceTrackerResult struct {
 	NumApplicationsProcessed int
 	NumErrors                int
-}
-
-// Basic wrapper struct for ArgoCD client options
-type ClientOptions struct {
-	ServerAddr      string
-	Insecure        bool
-	Plaintext       bool
-	Certfile        string
-	GRPCWeb         bool
-	GRPCWebRootPath string
-	AuthToken       string
-}
-
-// NewAPIClient creates a new API client for ArgoCD and connects to the ArgoCD Server
-func NewAPIClient(opts *ClientOptions) (ArgoCD, error) {
-
-	envAuthToken := os.Getenv("ARGOCD_TOKEN")
-	if envAuthToken != "" && opts.AuthToken == "" {
-		opts.AuthToken = envAuthToken
-	}
-
-	rOpts := argocdclient.ClientOptions{
-		ServerAddr:      opts.ServerAddr,
-		PlainText:       opts.Plaintext,
-		Insecure:        opts.Insecure,
-		CertFile:        opts.Certfile,
-		GRPCWeb:         opts.GRPCWeb,
-		GRPCWebRootPath: opts.GRPCWebRootPath,
-		AuthToken:       opts.AuthToken,
-	}
-	client, err := argocdclient.NewClient(&rOpts)
-	if err != nil {
-		return nil, err
-	}
-	return &argoCD{Client: client}, nil
 }
 
 func FilterApplicationsByArgoCDNamespace(apps []v1alpha1.Application, namespace string) []v1alpha1.Application {
@@ -81,29 +41,16 @@ func FilterApplicationsByArgoCDNamespace(apps []v1alpha1.Application, namespace 
 	return filteredApps
 }
 
-// ListApplications returns a list of all application names that the API user
-// has access to.
-func (client *argoCD) ListApplications() ([]v1alpha1.Application, error) {
-	conn, appClient, err := client.Client.NewApplicationClient()
-	if err != nil {
-		return nil, err
-	}
-	defer conn.Close()
-	apps, err := appClient.List(context.TODO(), &application.ApplicationQuery{})
-	if err != nil {
-		return nil, err
-	}
-	return apps.Items, nil
-}
-
 // Kubernetes based client
-type k8sClient struct {
-	kubeClient *kube.ResourceTrackerKubeClient
+type argocd struct {
+	kubeClient           *kube.KubeClient
+	ApplicationClientSet versioned.Interface
+	resourceMapperStore  map[string]*resourcegraph.ResourceMapper
 }
 
 // ListApplications lists all applications across all namespaces.
-func (client *k8sClient) ListApplications() ([]v1alpha1.Application, error) {
-	list, err := client.kubeClient.ApplicationClientSet.ArgoprojV1alpha1().Applications(v1.NamespaceAll).List(context.TODO(), v1.ListOptions{})
+func (argocd *argocd) ListApplications() ([]v1alpha1.Application, error) {
+	list, err := argocd.ApplicationClientSet.ArgoprojV1alpha1().Applications(v1.NamespaceAll).List(context.TODO(), v1.ListOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("error listing applications: %w", err)
 	}
@@ -113,31 +60,31 @@ func (client *k8sClient) ListApplications() ([]v1alpha1.Application, error) {
 
 // NewK8SClient creates a new kube client to interact with kube api-server.
 func NewK8SClient(kubeClient *kube.ResourceTrackerKubeClient) (ArgoCD, error) {
-	return &k8sClient{kubeClient: kubeClient}, nil
+	return &argocd{kubeClient: kubeClient.KubeClient, ApplicationClientSet: kubeClient.ApplicationClientSet}, nil
 }
 
-func ProcessApplication(app v1alpha1.Application, cfg *kube.ResourceTrackerKubeClient) error {
+func (argocd *argocd) ProcessApplication(app v1alpha1.Application) error {
 	// Fetch resource-relation-lookup ConfigMap
-	configMap, err := cfg.KubeClient.Clientset.CoreV1().ConfigMaps(app.Status.ControllerNamespace).Get(
+	configMap, err := argocd.kubeClient.Clientset.CoreV1().ConfigMaps(app.Status.ControllerNamespace).Get(
 		context.Background(), "resource-relation-lookup", v1.GetOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to fetch resource-relation-lookup ConfigMap: %w", err)
 	}
 	resourceRelations := configMap.Data
 	// Fetch AppProject
-	appProject, err := cfg.ApplicationClientSet.ArgoprojV1alpha1().AppProjects(app.Status.ControllerNamespace).Get(
+	appProject, err := argocd.ApplicationClientSet.ArgoprojV1alpha1().AppProjects(app.Status.ControllerNamespace).Get(
 		context.Background(), app.Spec.Project, v1.GetOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to fetch AppProject %s: %w", app.Spec.Project, err)
 	}
 	// Get RepoServer address
-	repoServerAddress, err := getRepoServerAddress(cfg.KubeClient.Clientset, app.Status.ControllerNamespace)
+	repoServerAddress, err := getRepoServerAddress(argocd.kubeClient.Clientset, app.Status.ControllerNamespace)
 	if err != nil {
 		return fmt.Errorf("failed to get repo server address: %w", err)
 	}
 	// Initialize required services
-	settingsManager := settings.NewSettingsManager(context.Background(), cfg.KubeClient.Clientset, app.Status.ControllerNamespace)
-	db := db.NewDB(app.Status.ControllerNamespace, settingsManager, cfg.KubeClient.Clientset)
+	settingsManager := settings.NewSettingsManager(context.Background(), argocd.kubeClient.Clientset, app.Status.ControllerNamespace)
+	db := db.NewDB(app.Status.ControllerNamespace, settingsManager, argocd.kubeClient.Clientset)
 	repoClientset := apiclient.NewRepoServerClientset(repoServerAddress, 120, apiclient.TLSConfiguration{DisableTLS: false, StrictValidation: false})
 	// Get child manifests
 	targetObjs, destinationConfig, err := GetApplicationChildManifests(context.Background(), &app, appProject, app.Status.ControllerNamespace, db, settingsManager, repoClientset, kubeutil.NewKubectl())
@@ -155,7 +102,24 @@ func ProcessApplication(app v1alpha1.Application, cfg *kube.ResourceTrackerKubeC
 	}
 	// If missing resources are found, update the resource relations
 	if needsUpdate {
-		resourceRelations, err = updateResourceRelationLookup(destinationConfig, app.Status.ControllerNamespace, cfg.KubeClient.Clientset)
+		if argocd.resourceMapperStore == nil {
+			argocd.resourceMapperStore = make(map[string]*resourcegraph.ResourceMapper)
+		}
+		mapper, exists := argocd.resourceMapperStore[destinationConfig.ServerName]
+		if !exists {
+			var err error
+			mapper, err = resourcegraph.NewResourceMapper(destinationConfig)
+			if err != nil {
+				return fmt.Errorf("failed to create ResourceMapper: %w", err)
+			}
+			argocd.resourceMapperStore[destinationConfig.ServerName] = mapper
+		}
+		if mapper.RequireInit() {
+			if err := mapper.Init(); err != nil {
+				return fmt.Errorf("failed to initialize ResourceMapper: %w", err)
+			}
+		}
+		resourceRelations, err = updateResourceRelationLookup(mapper, app.Status.ControllerNamespace, argocd.kubeClient.Clientset)
 		if err != nil {
 			klog.Errorf("failed to update resource-relation-lookup ConfigMap: %v", err)
 			return err
@@ -163,7 +127,7 @@ func ProcessApplication(app v1alpha1.Application, cfg *kube.ResourceTrackerKubeC
 	}
 	// Discover parent-child relationships
 	parentChildMap := kube.GetResourceRelation(resourceRelations, targetObjs)
-	return UpdateResourceInclusion(parentChildMap, cfg.KubeClient.Clientset, app.Status.ControllerNamespace)
+	return UpdateResourceInclusion(parentChildMap, argocd.kubeClient.Clientset, app.Status.ControllerNamespace)
 }
 
 func getRepoServerAddress(k8sclient kubernetes.Interface, controllerNamespace string) (string, error) {
